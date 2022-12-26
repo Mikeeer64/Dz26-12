91722a784cfd03e22a069052f01ac701156506bf
Mikeeer <my email>
2022-12-26 19:41:02 +0300

    2

---
+++ Example02/Program.cs
@@ -0,0 +1,12 @@
+﻿int GetNamber(int x, int y)
+{
+    int summ = 0;
+    if(x == y) return y;
+    else
+    {
+        summ = x + GetNamber(x + 1, y);
+    }
+    return summ;
+}
+
+Console.WriteLine(GetNamber(1, 15));
